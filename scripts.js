06b715d08db5188decd548577108e8af809426a7
Lansing Cai <lansingc418@gmail.com>
2016-05-26 00:02:52 -0700

    Add project cards to portfolio years

--- scripts.js
+++ scripts.js
@@ -1,27 +1,38 @@
-var topBarHeight = document.querySelector("#top-bar").offsetHeight;
-
-portfolioYear = document.querySelectorAll(".portfolio-year");
-
-var left = 0;
-[].forEach.call(portfolioYear, function (year) {
-	year.style.paddingTop = topBarHeight + 100 + "px";
-	year.style.width = window.offsetWidth + "px";
-
-	year.style.left = left;
-	left += year.offsetWidth;
-});
-
 $(function() {
+
+	var adjustForTopBar = function () {
+		var topBarHeight = document.querySelector("#top-bar").offsetHeight;
+
+		portfolioYear = document.querySelectorAll(".portfolio-year");
+
+		var left = 0;
+		[].forEach.call(portfolioYear, function (year) {
+			year.style.paddingTop = topBarHeight + "px";
+			year.style.width = window.offsetWidth + "px";
+
+			var projectsInYear = year.querySelectorAll(".project-card");
+			var portfolioWidth = year.offsetWidth;
+			var numberOfCardsThatFit = portfolioWidth / projectsInYear[0].offsetWidth;
+
+			if (numberOfCardsThatFit < projectsInYear.length) {
+				year.style.width = year.offsetWidth + (projectsInYear[0].offsetWidth * 0.8) + "px";
+			}
+
+			year.style.left = left;
+			left += year.offsetWidth;
+		});
+	}
+
+	$(window).resize(adjustForTopBar);
+	adjustForTopBar();
 
 	$("#scroll-bar").scrollLeft(10000);
 
-   $("body").mousewheel(function(event, delta) {
+	$("body").mousewheel(function(event, delta) {
+		var original = $("#scroll-bar").scrollLeft()
+		$("#scroll-bar").scrollLeft(original + delta);
 
-      var original = $("#scroll-bar").scrollLeft()
-      $("#scroll-bar").scrollLeft(original - delta * 2);
-    
-      event.preventDefault();
-
-   });
+		event.preventDefault();
+	});
 
 });
