364a0e97af55ec314d15c9f69faa72a2156883f5
Lansing Cai <lansingc418@gmail.com>
2016-05-26 20:39:35 -0700

    Add new font and changed body font, evened out the portfolio cards

--- scripts.js
+++ scripts.js
@@ -1,32 +1,52 @@
 $(function() {
 
-	var adjustForTopBar = function () {
-		var topBarHeight = document.querySelector("#top-bar").offsetHeight;
+	$("#top-bar").ready(function() {
+		var adjustForTopBar = function () {
+			var topBarHeight = document.querySelector("#top-bar").offsetHeight;
 
-		portfolioYear = document.querySelectorAll(".portfolio-year");
+			portfolioYear = document.querySelectorAll(".portfolio-year");
 
-		var left = 0;
-		[].forEach.call(portfolioYear, function (year) {
-			year.style.paddingTop = topBarHeight + "px";
-			year.style.width = window.offsetWidth + "px";
+			var left = 0;
+			[].forEach.call(portfolioYear, function (year) {
+				year.style.paddingTop = topBarHeight + "px";
+				year.style.width = window.offsetWidth + "px";
 
-			var projectsInYear = year.querySelectorAll(".project-card");
-			var portfolioWidth = year.offsetWidth;
-			var numberOfCardsThatFit = portfolioWidth / projectsInYear[0].offsetWidth;
+				var projectsInYear = year.querySelectorAll(".project-card");
+				var portfolioWidth = year.offsetWidth - 100;
+				var projectWidth = projectsInYear[0].offsetWidth + 20;
+				var projectWidth = 324;
+				var numberOfProjects = projectsInYear.length;
+				var numberOfProjectsThatFit = portfolioWidth / projectWidth;
 
-			if (numberOfCardsThatFit < projectsInYear.length) {
-				year.style.width = year.offsetWidth + (projectsInYear[0].offsetWidth * 0.8) + "px";
-			}
 
-			year.style.left = left;
-			left += year.offsetWidth;
-		});
-	}
+				console.log(projectsInYear, portfolioWidth, projectWidth, numberOfProjectsThatFit);
+				console.log(numberOfProjectsThatFit, numberOfProjects );
+				while (numberOfProjectsThatFit < numberOfProjects) {
+					year.style.width = year.offsetWidth + 1 + "px";
+					// year.style.width = year.offsetWidth + projectWidth + "px";
+					// year.style.width += ((1 - (numberOfProjects - numberOfProjectsThatFit)) * projectWidth);
 
-	$(window).resize(adjustForTopBar);
-	adjustForTopBar();
+					var projectsInYear = year.querySelectorAll(".project-card");
+					var portfolioWidth = year.offsetWidth - 50;
+					var projectWidth = projectsInYear[0].offsetWidth + 20;
+					var projectWidth = 324;
+					var numberOfProjects = projectsInYear.length;
+					var numberOfProjectsThatFit = portfolioWidth / projectWidth;
 
-	$("#scroll-bar").scrollLeft(10000);
+					console.log(projectsInYear, portfolioWidth, projectWidth, numberOfProjectsThatFit);
+					console.log(numberOfProjectsThatFit , numberOfProjects );
+				}
+
+				year.style.left = left;
+				left += year.offsetWidth;
+			});
+		}
+
+		adjustForTopBar();
+		$(window).resize(adjustForTopBar);
+
+		$("#scroll-bar").scrollLeft(10000);
+	})
 
 	$("body").mousewheel(function(event, delta) {
 		var original = $("#scroll-bar").scrollLeft()
